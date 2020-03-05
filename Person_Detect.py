65e2d40bd4ca8c81a84ca6aa4dadc5c720c1f47c
ashwins9001 <nitromekid@hotmail.com>
2020-03-05 06:40:48 +0530

    HOG+NMA person det
    Capture frame from live feed and run HOG with pre-trained pedestrian data, then NMS to reduce false positive bounding boxes.

---
+++ Person_Detect.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+from imutils.object_detection import non_max_suppression
+
+cap = cv2.VideoCapture(0)
+
+#Create HOG desc, import pretrained params
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+while True:
+    _, frame = cap.read()
+    #winStride determines x, y steps of sliding window for finding HOG, affects
+    #Accuracy and speed of algo
+    #Scale determines number of lvls in img pyramid, allows for dec size for inc res
+    #Could help determine grad dir accurately, at cost of proc time
+    (rects, weights) = hog.detectMultiScale(frame, winStride=(4,4),
+     padding=(8,8), scale=1.05)
+    for(x, y, w, h) in rects:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+    
+    #Apply NMS, loop over all bounding boxes, contain in arr
+    rects = np.array([[x, y, x+w, y+h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    #Draw rem bb
+    for(xA, yA, xB, yB) in pick:
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    
+    cv2.imshow("frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+    
